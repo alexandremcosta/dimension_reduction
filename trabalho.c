b40637120266f68de02a58abfe5b63b0ae68fcbb
Alexandre Costa <alexandremcost@gmail.com>
2017-04-13 17:44:42 -0300

    better memory allocation code

--- trabalho.c
+++ trabalho.c
@@ -3,8 +3,87 @@
 #include <math.h>
 #include <time.h>
 
-#define N_DOCS 3001
-#define N_VOCAB 102661
+// #define N_DOCS 3001 // slow
+// #define N_LINES 655468 //slow
+#define N_DOCS    101 // fast
+#define DOC_LINES 21853 // fast
+#define DOC_COLS  3
+#define N_VOCAB   102661
+
+void   read_file(char[], size_t, size_t, size_t, int[][DOC_COLS]);
+void*  create_bag();
+double dist_euclid(int[], int[]);
+void   brute_force_search(int (*table)[N_VOCAB]);
+void*  safe_alloc(int);
+
+int main()
+{
+    int(*bag)[N_VOCAB] = create_bag();
+    brute_force_search(bag); // 42 minutes
+
+    free(bag);
+    return 0;
+}
+
+void* create_bag()
+{
+    size_t docword_rows = DOC_LINES, docword_cols = DOC_COLS, docword_offset = 3; // small
+
+    int docword[docword_rows][docword_cols];
+    read_file("docword.nytimes.small.txt", docword_offset, docword_rows, docword_cols, docword);
+
+
+    int (*bag) [N_VOCAB] = safe_alloc(sizeof(int[N_DOCS][N_VOCAB]));
+    for ( int i = 0; i < N_DOCS; i++ )
+      for ( int j = 0; j < N_VOCAB; j++ )
+        bag[i][j] = 0;
+
+    for ( int i = 0; i < docword_rows; i++ )
+        bag[docword[i][0]][docword[i][1]] = (int) docword[i][2];
+
+    if ( bag[1][17654] == 4)
+      printf("Bag created\n");
+
+    return bag;
+}
+
+void brute_force_search(int (*table)[N_VOCAB])
+{
+    double (*distances) [N_DOCS] = safe_alloc(sizeof(double[N_DOCS][N_DOCS]));
+
+    clock_t begin = clock();
+
+    for (int i = 1; i < N_DOCS; i++)
+    {
+        // double progress = 100.0*i/N_DOCS;
+        // double elapsed = (double)(clock() - begin) / CLOCKS_PER_SEC;
+        // double total = elapsed*N_DOCS/i;
+        // printf("%.5f%% - Elapsed time: %.4f minutes - Remaining time: %.4f minutes \r", progress, elapsed/60, (total - elapsed)/60); fflush(stdout);
+
+        for (int j = i+1; j < N_DOCS; j++)
+        {
+            distances[i][j] = dist_euclid(table[i], table[j]);
+        }
+    }
+
+    double time_spent = (double)(clock() - begin) / CLOCKS_PER_SEC;
+
+    printf("\nBrute force time: %.4f minutes ", time_spent / 60);
+    if ( (int) distances[1][2] == 26 )
+      printf("OK!\n");
+    else
+      printf("Error!\n");
+
+    free(distances);
+}
+
+double dist_euclid(int a[], int b[])
+{
+    double sum = 0;
+    for ( int i = 0; i < N_VOCAB; i++ )
+        sum += (a[i] - b[i]) * (a[i] - b[i]);
+    return sqrt(sum);
+}
 
 void read_file(char filename[], size_t offset, size_t N, size_t M, int table[N][M])
 {
@@ -30,89 +109,13 @@ void read_file(char filename[], size_t offset, size_t N, size_t M, int table[N][
     }
 }
 
-int** allocate_bag()
+void* safe_alloc(int size)
 {
-    int** bag = malloc(N_DOCS * sizeof(int *));
-
-    for(int i = 0; i < N_DOCS; i++)
-    {
-        bag[i] = malloc(N_VOCAB * sizeof(int));
-        for(int j = 0; j < N_VOCAB; j++)
-            bag[i][j] = 0;
-    }
-
-    return bag;
+  void* space = malloc(size);
+  if (space == NULL)
+  {
+    printf("Memory allocation error!");
+    exit(0);
+  }
+  return space;
 }
-
-void free_bag(int** bag)
-{
-    for(int i = 0; i < N_DOCS; i++)
-        free(bag[i]);
-    free(bag);
-}
-
-int** create_bag()
-{
-    int** bag = allocate_bag();
-    size_t docword_rows = 655468, docword_cols = 3, docword_offset = 3;
-    int docword[docword_rows][docword_cols];
-
-    read_file("docword.nytimes.txt", docword_offset, docword_rows, docword_cols, docword);
-
-    for ( int i = 0; i < docword_rows; i++ )
-        bag[docword[i][0]][docword[i][1]] = (int) docword[i][2];
-
-    return bag;
-}
-
-double dist_euclid(int* a, int* b)
-{
-    double sum = 0;
-    for ( int i = 0; i < N_VOCAB; i++ )
-        sum += (a[i] - b[i]) * (a[i] - b[i]);
-    return sqrt(sum);
-}
-
-void brute_force_search(int** table)
-{
-    double** distances = malloc(N_DOCS * sizeof(double *));
-    for(int i = 0; i < N_DOCS; i++)
-        distances[i] = malloc(N_DOCS * sizeof(double));
-
-    clock_t begin = clock();
-
-    for (int i = 1; i < N_DOCS; i++)
-    {
-        for (int j = i+1; j < N_DOCS; j++)
-        {
-            distances[i][j] = dist_euclid(table[i], table[j]);
-        }
-    }
-
-    double time_spent = (double)(clock() - begin) / CLOCKS_PER_SEC;
-
-    printf("Brute force time: %.1f\n minutes", time_spent / 60);
-
-    for(int i = 0; i < N_DOCS; i++)
-        free(distances[i]);
-    free(distances);
-}
-
-int main()
-{
-    int** bag = create_bag();
-    brute_force_search(bag);
-
-    getchar();
-
-    free_bag(bag);
-    return 0;
-}
-
-
-      //  Time estimating
-      //  double progress = 100.0*i/N_DOCS;
-      //  double elapsed = (double)(clock() - begin) / CLOCKS_PER_SEC;
-      //  double total = elapsed*N_DOCS/i;
-      //  printf("%.5f %% - Elapsed time: %.2f - Remaining time: %.2f minutes \r", progress, elapsed, (total - elapsed)/60); fflush(stdout);
-
